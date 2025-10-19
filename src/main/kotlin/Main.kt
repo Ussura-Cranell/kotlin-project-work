11beb3a269bbc5e3ddad09d91229866af1e0e48b
Ussura <ussura@internet.ru>
2025-10-19 12:04:43 +0300

    feat: add Kotlin lambda system demonstration

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,12 @@ package core
 
 import core.Other.functionTesting
 import core.Other.classesTesting
+import core.Other.leetCodeSolution
+import core.Other.lambdaTesting
 
 fun main() {
 
     // functionTesting()
-    classesTesting()
-
+    // classesTesting()
+    lambdaTesting()
 }
